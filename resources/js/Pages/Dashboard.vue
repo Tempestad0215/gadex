0e8cad7bd14603562c26e035b5726947a1d93af2
Mario Guzman <marioguzman140@gmail.com>
2024-05-14 23:50:27 -0400

    creando el dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,69 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import {Head} from '@inertiajs/vue3';
+
+
+
 </script>
 
-<template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+
+
+<template>
+    <!-- Titulo de la ventana -->
+    <Head title="Dashboard" />
+
+    <!-- Contenido de la ventana -->
+    <div class=" flex h-screen">
+        <!-- Barra latera -->
+        <div class=" bg-blue-400 col-span-2 w-[200px] ">
+
+            <!-- Informacion de la empresa -->
+            <div class="py-3" >
+                <img
+                    class=" mx-auto"
+                    width="100"
+                    src="../../../public/storage/images/logogadex.png"
+                    alt="logo de la empresa Gadex">
+            </div>
+
+
+            <!-- Nombre de usuario y cargo -->
+            <div class=" text-center">
+                <h4 class=" text-xl font-bold">
+                    Marionil G.
+                </h4>
+                <span class=" text-sm -pt-2">
+                    (Recepcion)
+                </span>
+            </div>
+            <div>
+
+            </div>
+            <hr>
+            barra lateral
+        </div>
+        <!-- Contenido de la ventanae -->
+        <div class=" flex-1 flex flex-col ">
+            <!-- Cabecera -->
+            <div class=" bg-red-200 h-[65px] relative  ">
+
+
+                cabecera
+
+                <div class=" absolute right-0 inset-y-0 pr-5 flex items-center" >
+                    <i
+                        class="fa-solid fa-arrow-right-from-bracket text-3xl float-right "></i>
                 </div>
             </div>
+
+
+            <!-- Contenido de la ventanae -->
+            <div class=" bg-green-200 flex-1 p-5">
+                prueba
+            </div>
+
         </div>
-    </AppLayout>
+    </div>
+
+
 </template>
---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,109 @@
+<script setup>
+import { Head, Link, useForm } from '@inertiajs/vue3';
+
+
+
+// formulario para el envio de todo
+const form = useForm({
+    email:"",
+    password:"",
+    remember: false
+});
+
+
+// Enviar el formulario
+const submit = () =>{
+    form.post('/login',{
+        onSuccess:()=>{
+            console.log('enviado');
+        },
+        onError:()=>{
+            console.log('error');
+        }
+    })
+}
+
+
+</script>
+
+<template>
+    <Head title="Login" />
+
+    <!-- Contenido de la ventana -->
+    <div
+        class="login-container">
+
+        <!-- Contenido del formulario -->
+        <div
+            class=" bg-white min-h-svh  lg:max-w-md rounded-lg p-6 shadow-lg bg-opacity-60 ">
+            <div>
+                <h3 class=" text-3xl font-bold text-center ">
+                    Login
+                </h3>
+                <div>
+                    <img
+                        class=" mx-auto"
+                        width="150"
+                        src="../../../public/storage/images/logogadex.png"
+                        alt="logo de la empresa">
+                </div>
+            </div>
+            <form
+                @submit.prevent="submit">
+                <!-- Email del usaurio -->
+                <div>
+                    <label
+                        class="label block"
+                        for="email">
+                        Email
+                    </label>
+                    <input
+                        v-model="form.email"
+                        class="input w-full"
+                        type="email" required >
+                </div>
+
+                <!-- Password de uisuario -->
+                <div class=" mt-3">
+                    <label
+                        class="label block"
+                        for="password">
+                        Password
+                    </label>
+                    <input
+                        v-model="form.password"
+                        class="input w-full"
+                        type="password">
+                </div>
+
+
+                <!-- Recordar el usuiario -->
+                <div class=" mt-5">
+                    <input
+                        v-model="form.remember"
+                        class=" hidden peer"
+                        type="checkbox"
+                        name="remember"
+                        id="remember">
+                    <label
+                        class=" peer-checked:text-white peer-checked:bg-blue-800 bg-blue-300 py-1.5 rounded-2xl px-3 duration-300 font-bold "
+                        for="remember">
+                        Remember me
+                    </label>
+                </div>
+
+
+                <!-- Botones de usuario -->
+                <div class="mt-3 text-right">
+                    <button
+                        class="btn-send"
+                        type="submit">
+                        Sing in
+                    </button>
+                </div>
+
+
+            </form>
+        </div>
+    </div>
+</template>
